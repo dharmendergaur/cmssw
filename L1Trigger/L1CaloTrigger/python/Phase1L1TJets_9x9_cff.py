bdf9e0abd464aded3ce2c77565c5ab0d95f7b9e3
Emyr Clement <emyr.clement@bristol.ac.uk>
2021-11-10 14:58:34 +0000

    Emulation of sort and histo seeding
    Ntuple code.
    
    Emulation of sorting and histo seeding
    
    Jet sort matched firmware, need to tidy up.
    
    Changes for latest pattern files.
    
    First commit of histo jet seeding.
    
    Conflicts:
    	L1Trigger/L1CaloTrigger/python/Phase1L1TJets_9x9trimmed_cff.py

--- L1Trigger/L1CaloTrigger/python/Phase1L1TJets_9x9_cff.py
+++ L1Trigger/L1CaloTrigger/python/Phase1L1TJets_9x9_cff.py
@@ -15,11 +15,18 @@ Phase1L1TJetCalibrator9x9.inputCollectionTag = cms.InputTag("Phase1L1TJetProduce
 Phase1L1TJetCalibrator9x9.outputCollectionName = cms.string("Phase1L1TJetFromPfCandidates")
 
 Phase1L1TJetSumsProducer9x9 = Phase1L1TJetSumsProducer.clone(
-  inputJetCollectionTag = cms.InputTag("Phase1L1TJetCalibrator9x9", "Phase1L1TJetFromPfCandidates"),
+  inputJetCollectionTag = cms.InputTag("Phase1L1TJetCalibrator9x9Unsorted", "Phase1L1TJetFromPfCandidates"),
 )
+
+Phase1L1TJetCalibrator9x9Unsorted = Phase1L1TJetCalibrator9x9.clone(
+	  inputCollectionTag = cms.InputTag("Phase1L1TJetProducer9x9", "UncalibratedPhase1L1TJetFromPfCandidatesUnsorted", ""),
+    outputCollectionName = cms.string("Phase1L1TJetFromPfCandidates")
+)
+
 
 Phase1L1TJetsSequence9x9 = cms.Sequence(
   Phase1L1TJetProducer9x9 +
   Phase1L1TJetCalibrator9x9 + 
+  Phase1L1TJetCalibrator9x9Unsorted +
   Phase1L1TJetSumsProducer9x9
 )
--- L1Trigger/L1CaloTrigger/python/Phase1L1TJets_cff.py
+++ L1Trigger/L1CaloTrigger/python/Phase1L1TJets_cff.py
@@ -4,8 +4,14 @@ from L1Trigger.L1CaloTrigger.Phase1L1TJetProducer_cfi import Phase1L1TJetProduce
 from L1Trigger.L1CaloTrigger.Phase1L1TJetCalibrator_cfi import Phase1L1TJetCalibrator
 from L1Trigger.L1CaloTrigger.Phase1L1TJetSumsProducer_cfi import Phase1L1TJetSumsProducer
 
+Phase1L1TJetCalibratorUnsorted = Phase1L1TJetCalibrator.clone(
+	  inputCollectionTag = cms.InputTag("Phase1L1TJetProducer", "UncalibratedPhase1L1TJetFromPfCandidatesUnsorted", ""),
+    outputCollectionName = cms.string("Phase1L1TJetFromPfCandidates")
+)
+
 Phase1L1TJetsSequence = cms.Sequence(
   Phase1L1TJetProducer +
   Phase1L1TJetCalibrator +
+  Phase1L1TJetCalibratorUnsorted +
   Phase1L1TJetSumsProducer
 )
